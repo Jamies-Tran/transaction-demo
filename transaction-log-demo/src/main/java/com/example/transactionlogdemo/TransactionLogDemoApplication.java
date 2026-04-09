e6375b49442b0f006810a46ad8e27df26a97e2dd
minhtq <minhtranquang2197@gmail.com>
2026-04-09 20:37:21 +0700

    first commit

---
+++ transaction-log-demo/src/main/java/com/example/transactionlogdemo/TransactionLogDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.transactionlogdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransactionLogDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransactionLogDemoApplication.class, args);
+    }
+
+}
---
+++ transaction-log-demo/src/test/java/com/example/transactionlogdemo/TransactionLogDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.transactionlogdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransactionLogDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
